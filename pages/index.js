696bddc2e0241af2027545b992a821a043a61274
Cody Clark <codylcl@yahoo.com>
2023-03-28 17:32:22 -1000

    header layout title and icons

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,11 +16,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className=' bg-blue-700 min-h-screen'>
-        Cody Clark
+      <main className="bg-black px-10">
+        <section className="min-h-screen">
+          <nav className=" py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">codedByCody</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-point text-xl"/>
+                </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-md ml-8" href='#'>Resume</a>
+                </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Cody Clark</h2>
+            <h3 className="text-2xl py-2">Software Developer</h3>
+            <p className="text-md py-5 leading- text-gray-800">Text Here</p>
+          </div>
+          <div className="flex justify-center gap-16 py-3 text-gray-600 text-5xl">
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div><img></img></div>
         </section>
       </main>
     </div>
-  )
+  );
 }
